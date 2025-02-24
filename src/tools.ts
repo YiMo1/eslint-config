6e1d245939d7132a68015dfefb59c270842748dd
伊墨 <1905672206@qq.com>
2025-02-24 16:29:36 +0800

    feat: 添加tools.ts文件

---
+++ src/tools.ts
@@ -0,0 +1,32 @@
+import type { Linter } from 'eslint'
+
+export function renameRules(
+  rules: Linter.RulesRecord,
+  map: Record<string, string>,
+): Linter.RulesRecord {
+  return Object.fromEntries(
+    Object.entries(rules).map(([key, value]) => {
+      for (const [from, to] of Object.entries(map)) {
+        if (key.startsWith(`${from}/`)) return [to + key.slice(from.length), value]
+      }
+      return [key, value]
+    }),
+  )
+}
+
+function isObject(val: unknown) {
+  return typeof val == 'object' && val != null
+}
+
+export function overrides<T extends object>(target: T, source: T): T {
+  for (const key of Object.keys(source)) {
+    const targetVal = target[key as keyof T]
+    const sourceVal = source[key as keyof T]
+    if (isObject(targetVal) && isObject(sourceVal)) {
+      overrides(targetVal, sourceVal)
+    } else {
+      target[key as keyof T] = sourceVal
+    }
+  }
+  return target
+}
