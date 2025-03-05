34f5a4a6a6d164ebcf9ab370603c7f05ecf1bf81
伊墨 <1905672206@qq.com>
2025-03-05 15:38:14 +0800

    perf(tailwindcss): 删除未开启的规则

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -7,12 +7,9 @@ export function tailwindcss(): Linter.Config {
     plugins: { tailwindcss: plugin as ESLint.Plugin },
     rules: {
       'tailwindcss/enforces-shorthand': 'error',
-      'tailwindcss/no-custom-classname': 'off',
       'tailwindcss/enforces-negative-arbitrary-values': 'error',
       'tailwindcss/classnames-order': 'error',
       'tailwindcss/no-unnecessary-arbitrary-value': 'error',
-      'tailwindcss/no-arbitrary-value': 'off',
-      'tailwindcss/migration-from-tailwind-2': 'off',
       'tailwindcss/no-contradicting-classname': 'error',
     },
   }
