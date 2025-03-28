cd3646b8f7a9ca22c4856d624ef13e281597e0ab
伊墨 <1905672206@qq.com>
2025-03-28 09:25:59 +0800

    feat: export all plugin

---
+++ src/plugin.ts
@@ -0,0 +1,8 @@
+import stylistic from '@stylistic/eslint-plugin'
+import importX from 'eslint-plugin-import-x'
+import tailwindcss from 'eslint-plugin-tailwindcss'
+import vue from 'eslint-plugin-vue'
+import globals from 'globals'
+import ts from 'typescript-eslint'
+
+export { stylistic, importX, tailwindcss, vue, globals, ts }
