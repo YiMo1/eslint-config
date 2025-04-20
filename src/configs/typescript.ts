230d94d8e967bf4ab6002bee383da1cd23abdf0c
伊墨 <1905672206@qq.com>
2025-04-20 09:55:13 +0800

    refactor: remove ban-ts-comment

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -70,7 +70,6 @@ export function typescript(options: TypescriptOptions = {}): Linter.Config[] {
         'ts/no-namespace': 'error',
         'ts/adjacent-overload-signatures': 'error',
         'ts/array-type': 'error',
-        'ts/ban-ts-comment': 'error',
         'ts/ban-tslint-comment': 'error',
         'ts/class-literal-property-style': 'error',
         'ts/consistent-generic-constructors': 'error',
