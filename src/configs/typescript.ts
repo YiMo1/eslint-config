41da2494bdd360133ccc45828f563de11e704f35
伊墨 <1905672206@qq.com>
2025-05-17 19:21:49 +0800

    refactor: remove `ts/no-extraneous-class`

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -82,7 +82,6 @@ export function typescript(options: TypescriptOptions = {}): Linter.Config[] {
         'ts/no-duplicate-type-constituents': 'error',
         'ts/no-dynamic-delete': 'error',
         'ts/no-extra-non-null-assertion': 'error',
-        'ts/no-extraneous-class': 'error',
         'ts/no-import-type-side-effects': 'error',
         'ts/no-inferrable-types': 'error',
         'ts/no-misused-new': 'error',
